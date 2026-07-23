f574cfba1dced9554a0f7b6d264c7bc537a97e74
Thiago Hersan <thiago.hersan+github@gmail.com>
2026-07-23 00:59:53 -0400

    update clustering with umap

--- js/cluster.js
+++ js/cluster.js
@@ -3,15 +3,15 @@ function filterByCluster(ids) {
   const clusterSelectionEl = document.getElementById("cluster--selection");
   const selectedCluster = clusterSelectionEl.getAttribute("data-cluster");
 
-  if (!clusterData || clusterCountEl.valueAsNumber < 2) return ids;
+  if (!clusterData || clusterCountEl.valueAsNumber < 7) return ids;
 
-  return new Set(Array.from(ids).filter(id => clusterData[clusterCountEl.value]["images"][id]["cluster"] == selectedCluster));
+  return new Set(Array.from(ids).filter(id => clusterData[clusterCountEl.value]["umap"]["images"][id]["cluster"] == selectedCluster));
 }
 
 function byDistFromCluster(clusterIdx, clusterCount) {
   const byClusterDist = (a, b) => {
-    const aDist = clusterData[clusterCount]["images"][a]["distances"][clusterIdx];
-    const bDist = clusterData[clusterCount]["images"][b]["distances"][clusterIdx];
+    const aDist = clusterData[clusterCount]["umap"]["images"][a]["distances"][clusterIdx];
+    const bDist = clusterData[clusterCount]["umap"]["images"][b]["distances"][clusterIdx];
     return aDist - bDist;
   };
   return byClusterDist;
@@ -22,12 +22,12 @@ function sortByCluster(ids) {
   const clusterSelectionEl = document.getElementById("cluster--selection");
   const selectedCluster = clusterSelectionEl.getAttribute("data-cluster");
 
-  if (!clusterData || clusterCountEl.valueAsNumber < 2) return ids;
+  if (!clusterData || clusterCountEl.valueAsNumber < 7) return ids;
 
   return ids.toSorted(byDistFromCluster(selectedCluster, clusterCountEl.value));
 }
 
 function getClusterDescription(numClusters, clusterIdx) {
-  const clusterDescription = clusterData[numClusters]["clusters"]["descriptions"]["gemma3"]["pt"][clusterIdx];
+  const clusterDescription = clusterData[numClusters]["umap"]["clusters"]["descriptions"]["gemma3"]["pt"][clusterIdx];
   return clusterDescription.join(", ");
 }
